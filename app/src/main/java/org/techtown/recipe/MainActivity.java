d27194d35e50b1b7f35201bae026f2819be68034
lsy <bogogae@naver.com>
2019-09-19 04:44:32 +0900

    19.09.19 // 레시피 화면 리스트뷰구현

---
+++ app/src/main/java/org/techtown/recipe/MainActivity.java
@@ -0,0 +1,26 @@
+package org.techtown.recipe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1=(Button)findViewById(R.id.start);
+        button1.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(),Menu.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/org/techtown/recipe/Menu.java
@@ -0,0 +1,26 @@
+package org.techtown.recipe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button2=(Button)findViewById(R.id.inform);
+        button2.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(),inform.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
