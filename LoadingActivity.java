1cd9945534abd1ad4d2c87b4043bb8c028850554
Kimyeongsan <52697123+Kimyeongsan@users.noreply.github.com>
2019-09-21 15:19:13 +0900

    Add files via upload

---
+++ LoadingActivity.java
@@ -0,0 +1,22 @@
+package org.techtown.recipe;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,39 @@
+package org.techtown.recipe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.text.SimpleDateFormat;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this,LoadingActivity.class));
+
+        TextView time = findViewById(R.id.text_time);
+
+        Long date = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd EEE");
+
+        String getTime = sdf.format(date);
+
+        time.setText("Today"+getTime);
+
+        Button infoButton=findViewById(R.id.informbutton);
+        infoButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), foodlistActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
