62dbb3229ef340de1caaf0f4b3f5eadcf660daa9
Kimyeongsan <apua34@gmail.com>
2019-09-22 17:39:05 +0900

    Loading Activity

--- app/src/main/java/org/techtown/recipe/MainActivity.java
+++ app/src/main/java/org/techtown/recipe/MainActivity.java
@@ -5,22 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.text.SimpleDateFormat;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this,LoadingActivity.class));
 
-//        Button button1=(Button)findViewById(R.id.start);
-//        button1.setOnClickListener(
-//                new Button.OnClickListener(){
-//                    public void onClick(View view){
-//                        Intent intent=new Intent(getApplicationContext(),Menu.class);
-//                        startActivity(intent);
-//                    }
-//                }
-//        );
+        TextView time = findViewById(R.id.text_time);
+
+        Long date = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd EEE");
+
+        String getTime = sdf.format(date);
+
+        time.setText("Today"+getTime);
+
+        Button infoButton=findViewById(R.id.informbutton);
+        infoButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View view){
+                        Intent intent=new Intent(getApplicationContext(), foodlistActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
